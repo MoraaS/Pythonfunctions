1e842b17debf896ce14aa045fe161705ff690d69
MoraaS <salmamoraa@gmail.com>
2019-01-16 09:25:29 +0000

    Python functions file

---
+++ functions.py
@@ -0,0 +1,28 @@
+#functions are a group of code and set of instructions
+def function1 () :
+    print("ayeeee")
+    print("ehahah")
+print("outside the function")
+function1 ()
+function1 ()
+function1 ()
+#functions are also defined as mapping
+#can be an inpu or argument
+def function2 (x):
+    return 2*x
+a = function2 (3)
+# 6 in this case will be the return value or output
+print (a)
+b = function2 (4)
+print(b)
+#third function
+def function3 (x , y) :
+    return x + y
+e = function3 (3 , 5)
+print(e)
+
+
+
+
+
+
